59c59d59cec51af9ae8070fd3c5bb775e75ea40d
UBaksa <ujkanovicbakir03@gmail.com>
2023-03-17 14:54:58 +0100

    Jonas - 010 Op Precedence

---
+++ Fundamental parts part1/jsjs.js
@@ -0,0 +1,95 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let person = "jonas";
+// let PI = 3.1414;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
+
+// // console.log(typeof null); //objekat,jer je na samom pocetku JS-a null bio objekat pa nisu mogli da promene to jer bi crashalo.
+// //
+
+// let age = 30;
+// age = 31; //we called it reinicijalizacija
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// // const job;
+
+// var job = "programmer";
+// job = "teacher";
+
+// // lastName = "Schmedtmann";
+// // console.log(lastName);
+
+// //
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means to the power of 3 = 2*2*2=8
+
+// const firstName = "jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5; //15
+// x += 10; //x=x+10;
+// console.log(x); //25
+// x *= 4; //x=x*4=100
+// x++;
+// x--;
+// console.log(x);
+
+// //Comparison operators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 20);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2020);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = 30 + 27 / 2;
+console.log(averageAge);
