a582ab45b961ff76496312b06346e1e8155d8016
UBaksa <ujkanovicbakir03@gmail.com>
2023-04-01 13:58:43 +0200

    guess my number 95% done

--- guess my number/app.js
+++ guess my number/app.js
@@ -1,7 +1,7 @@
 let dugme = document.getElementById("btn");
 let output = document.getElementById("outputtext");
 
-let number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.floor(Math.random() * 20) + 1;
 console.log(number);
 
 let score = 20;
@@ -18,17 +18,40 @@ dugme.addEventListener("click", function () {
     document.body.style.backgroundColor = "green";
     output.innerHTML = `✅Pogodili ste broj,to je broj ${number} !`;
     trpoeni.innerHTML = `🔱Trenutni poeni:${score}`;
-    makspoeni.innerHTML = `🪙Maksimalni poeni:${makspoeni}`;
+    makspoeni.innerHTML = `🪙Maksimalni poeni:${highscore}`;
+    if (highscore < score) {
+      highscore = score;
+      trpoeni.innerHTML = `🔱Trenutni poeni:${score}`;
+      makspoeni.innerHTML = `🪙Maksimalni poeni:${highscore}`;
+    }
   } else if (input < number) {
+    trpoeni.innerHTML = `🔱Trenutni poeni:${score}`;
+    makspoeni.innerHTML = `🪙Maksimalni poeni:${highscore}`;
     output.innerHTML = "❌Vas broj je manji od zamisljenog!";
   }
   if (input > number) {
+    trpoeni.innerHTML = `🔱Trenutni poeni:${score}`;
+    makspoeni.innerHTML = `🪙Maksimalni poeni:${highscore}`;
     output.innerHTML = "❌Vas broj je veci od zamisljenog!";
   }
 });
 
-let background1 = document.getElementsByClassName("div1");
-let background2 = document.getElementsByClassName("div2");
+let again = document.getElementById("again");
+
+again.addEventListener("click", function (e) {
+  e.preventDefault();
+  number = Math.floor(Math.random() * 20) + 1;
+  console.log(number);
+  score = 20;
+  document.body.style.backgroundColor = "black";
+  trpoeni.innerHTML = `🔱Trenutni poeni:${score}`;
+  makspoeni.innerHTML = `🪙Maksimalni poeni:${highscore}`;
+  output.innerHTML = "Unesite broj!";
+  document.querySelector("input").value = "";
+});
+
+let background1 = document.getElementsByClassName("div1")[0];
+let background2 = document.getElementsByClassName("div2")[0];
 let h1 = document.getElementsByTagName("h1");
 let h3 = document.getElementsByTagName("h3");
 let p = document.getElementsByTagName("p");
@@ -37,27 +60,26 @@ let doleinput = document.getElementById("kontainer");
 console.log(background1);
 console.log(background2);
 
-// background1.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   document.body.style.backgroundColor = "black";
-//   h1.forEach((e) => {
-//     e.style.color = "ghostwhite";
-//   });
-//   h3.forEach((e) => {
-//     e.style.color = "ghostwhite";
-//   });
-//   naslov.style.backgroundColor = "rgb(61, 59, 59)";
-//   doleinput.style.backgroundColor = "rgb(61, 59, 59)";
-// });
+background1.addEventListener("click", function (e) {
+  document.body.style.backgroundColor = "black";
+  Array.from(h1).forEach((e) => {
+    e.style.color = "ghostwhite";
+  });
+  Array.from(h3).forEach((e) => {
+    e.style.color = "ghostwhite";
+  });
+  naslov.style.backgroundColor = "rgb(61, 59, 59)";
+  doleinput.style.backgroundColor = "rgb(61, 59, 59)";
+});
 
-// background2.addEventListener("click", function () {
-//   document.body.style.backgroundColor = "white";
-//   h1.forEach((e) => {
-//     e.style.color = "ghostwhite";
-//   });
-//   h3.forEach((e) => {
-//     e.style.color = "ghostwhite";
-//   });
-//   naslov.style.backgroundColor = "rgb(61, 59, 59)";
-//   doleinput.style.backgroundColor = "rgb(61, 59, 59)";
-// });
+background2.addEventListener("click", function () {
+  document.body.style.backgroundColor = "gainsboro";
+  Array.from(h1).forEach((e) => {
+    e.style.color = "black";
+  });
+  Array.from(h3).forEach((e) => {
+    e.style.color = "black";
+  });
+  naslov.style.backgroundColor = "rgb(61, 59, 59)";
+  doleinput.style.backgroundColor = "rgb(61, 59, 59)";
+});
